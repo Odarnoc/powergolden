79701e151bef3cff0e4cafd7df4ba8d81ad6a2fc
Juan Carlos Berra Sanchez <jcberra@outlook.com>
2019-12-09 11:07:18 -0600

    phpmailer

--- ajax/recuperar_contrasena.php
+++ ajax/recuperar_contrasena.php
@@ -1,12 +1,41 @@
 <?php
-ini_set( 'display_errors', 1 );
-    error_reporting( E_ALL );
-    $from = "test@hostinger-tutorials.com";
-    $to = "test@gmail.com";
-    $subject = "Checking PHP mail";
-    $message = "PHP mail works just fine";
-    $headers = "From:" . $from;
-    mail($to,$subject,$message, $headers);
-    echo "The email message was sent.";
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
 
-?>
+require '../phpMailer/Exception.php';
+require '../phpMailer/PHPMailer.php';
+require '../phpMailer/SMTP.php';
+
+$mail = new PHPMailer(true);
+
+try {
+    //Server settings
+   $mail->SMTPDebug = 2;                      // Enable verbose debug output
+    $mail->isSMTP();                                            // Send using SMTP
+    $mail->Host       = 'smtp1.gmail.com';                    // Set the SMTP server to send through
+    $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
+    $mail->Username   = 'ejemploberras@gmail.com';                     // SMTP username
+    $mail->Password   = 'berras12345';                               // SMTP password
+    $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;         // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` also accepted
+    $mail->Port       = 587;                                    // TCP port to connect to
+
+    //Recipients
+    $mail->setFrom('ejemploberras@gmail.com', 'Carlos Berra');
+    $mail->addAddress('jcberra@outlook.com', 'Berra');     // Add a recipient
+
+    /*// Attachments
+    $mail->addAttachment('/var/tmp/file.tar.gz');         // Add attachments
+    $mail->addAttachment('/tmp/image.jpg', 'new.jpg');    // Optional name
+    */
+
+    // Content
+    $mail->isHTML(true);                                  // Set email format to HTML
+    $mail->Subject = 'Prueba';
+    $mail->Body    = 'Correo de Prueba';
+    $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+
+    $mail->send();
+    echo 'El correo se envio con exito';
+} catch (Exception $e) {
+    echo "No se pudo enviar el correo: {$mail->ErrorInfo}";
+}
