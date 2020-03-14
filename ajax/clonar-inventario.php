d5b655e8c076e8632e824e99577a181c63806a9f
Agustin Sandoval Becerra <asandoval@ant.com.mx>
2020-03-13 23:50:07 -0600

    inventario

---
+++ ajax/clonar-inventario.php
@@ -0,0 +1,50 @@
+<?php
+require '../bd/conexion.php';
+require '../utils/error.php';
+
+$response['mensaje'] = "Exito al crear usuario.";
+
+if(!isset($_POST['sucursal'])&&!isset($_POST['sucursal_clonar'])){
+    error_mensaje("Completar todos los campos.");
+    return;
+}
+
+if(empty($_POST['sucursal'])){
+    error_mensaje('Llenar sucursal.');
+    return;
+}
+if(empty($_POST['sucursal_clonar'])){
+    error_mensaje('Llenar sucursal a clonar.');
+    return;
+}
+
+    $sucursal = $_POST['sucursal'];
+    $sucursal_clonar = $_POST['sucursal_clonar'];
+
+    $inventario = R::find( 'inventarios', 'sucursal_id = ?', [ $sucursal_clonar ]);
+
+    foreach ($inventario as $valor) {
+        $producto = $valor->producto_id;
+        $minimo= $valor->limite_inventario;
+        $existencias= $valor->existencia;
+        $existe = R::findOne( 'inventarios', ' producto_id = ? && sucursal_id = ?', [ $producto, $sucursal ]);
+
+        if(empty($existe)){
+            $registro = R::dispense('inventarios');
+            $registro->sucursal_id = $sucursal;
+            $registro->limite_inventario = $minimo;
+            $registro->producto_id = $producto;
+            $registro->existencia = $existencias;
+            $id = R::store($registro);
+        }else{
+            $existe->sucursal_id = $sucursal;
+            $existe->limite_inventario = $minimo;
+            $existe->producto_id = $producto;
+            $existe->existencia += $existencias;
+            $id = R::store($existe);
+        }
+    }
+
+    echo json_encode($response);
+    
+?>
